4bf02fe6d0e1e5d306ac8a1d8e6b01ba5b4c9125
Erik Michaels-Ober <sferik@gmail.com>
2012-11-15 16:22:36 -0800

    Convert specs to the new RSpec expectation syntax
    See http://myronmars.to/n/dev-blog/2012/06/rspecs-new-expectation-syntax

--- spec/omniauth/strategies/twitter_spec.rb
+++ spec/omniauth/strategies/twitter_spec.rb
@@ -5,17 +5,17 @@ describe OmniAuth::Strategies::Twitter do
     OmniAuth::Strategies::Twitter.new({})
   end
 
-  context "client options" do
+  context 'client options' do
     it 'should have correct name' do
-      subject.options.name.should eq("twitter")
+      expect(subject.options.name).to eq('twitter')
     end
 
     it 'should have correct site' do
-      subject.options.client_options.site.should eq('https://api.twitter.com')
+      expect(subject.options.client_options.site).to eq('https://api.twitter.com')
     end
 
     it 'should have correct authorize url' do
-      subject.options.client_options.authorize_path.should eq('/oauth/authenticate')
+      expect(subject.options.client_options.authorize_path).to eq('/oauth/authenticate')
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,5 +12,7 @@ RSpec.configure do |config|
   config.include WebMock::API
   config.include Rack::Test::Methods
   config.extend  OmniAuth::Test::StrategyMacros, :type => :strategy
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
 end
-
